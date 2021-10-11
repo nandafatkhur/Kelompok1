5ebff2f7e90559612f732d3bd020b273e0adde03
CitaPuspita <91959390+CitaPuspita@users.noreply.github.com>
2021-10-11 22:27:32 +0700

    Update Prisma.java

--- Ruang/src/Prisma.java
+++ Ruang/src/Prisma.java
@@ -1,19 +1,25 @@
 package Ruang;
 import java.util.Scanner;
-public class Prisma {
-    public static void main (String [] args) {
+public class Prisma extends BangunRuang{
+    int alas , tinggi1 , tinggi2 ;
+    public Prisma(){
+        System.out.println("Menghitung Prisma");
         Scanner nilai = new Scanner (System.in);
-        int alas , tinggi1 , tinggi2 ;
-        System.out.println("Masukan Alas Segitiga = ");
+        System.out.print("\tMasukan Alas Segitiga : ");
         alas = nilai.nextInt();
-        System.out.println("Masukan Tinggi Segitiga = ");
+        System.out.print("\tMasukan Tinggi Segitiga : ");
         tinggi1 = nilai.nextInt();
-        System.out.println("Masukan Tinggi Prisma = ");
+        System.out.print("\tMasukan Tinggi Prisma : ");
         tinggi2 = nilai.nextInt();
-        double Luas , Volume ;
-        Volume = 0.5 * alas * tinggi1 * tinggi2;
-        System.out.println("Volume Prisma adalah = "+Volume);
-        Luas = tinggi2 * (3 * alas) + (2 * (0.5 * alas * tinggi1));
-        System.out.println("Luas Permukaan Prisma adalah = "+Luas);
+    }
+
+    @Override
+    public double volume() {
+        return 0.5 * this.alas * this.tinggi1 * this.tinggi2;
+    }
+
+    @Override
+    public double luasPermukaan() {
+        return this.tinggi2 * (3 * this.alas) + (2 * (0.5 * this.alas * this.tinggi1));
     }
 }
