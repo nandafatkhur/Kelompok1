b4cc831049d43c3c31c9116468a8d9c3ae08199d
nandafatkhur <nandafatkhur@gmail.com>
2021-10-11 17:22:11 +0700

    Update Bola.java

--- Ruang/src/Bola.java
+++ Ruang/src/Bola.java
@@ -1,15 +1,21 @@
-package Ruang;
 import java.util.Scanner;
-public class Bola {
-    public static void main (String [] args) {
-        Scanner nilai = new Scanner (System.in);
-        double phi= 3.14 , jari ;
-        System.out.println("Masukan Jari-Jari = ");
+
+public class Bola extends BangunRuang{
+    double jari , phi=3.14;
+    public Bola(){
+        System.out.println("Menghitung Bola");
+        Scanner nilai = new Scanner(System.in);
+        System.out.print("\tMasukkan Jari-jari : ");
         jari = nilai.nextInt();
-        double Luas , Volume ;
-        Volume = 4/ (double) 3 * phi * jari * jari *jari;
-        System.out.println("Volume Bola adalah = "+Volume);
-        Luas = 4 * phi * jari * jari;
-        System.out.println("Luas Permukaan Bola adalah = "+Luas);
+    }
+
+    @Override
+    public double volume() {
+        return 4/ (double) 3 * this.phi * this.jari * this.jari * this.jari;
+    }
+
+    @Override
+    public double luasPermukaan() {
+        return 4 * this.phi * this.jari * this.jari;
     }
 }
