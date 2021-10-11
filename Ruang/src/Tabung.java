0eaa878a05801c083640759300392ca1d5f5cf3e
CitaPuspita <91959390+CitaPuspita@users.noreply.github.com>
2021-10-11 22:23:10 +0700

    Update Tabung.java

--- Ruang/src/Tabung.java
+++ Ruang/src/Tabung.java
@@ -1,17 +1,25 @@
 package Ruang;
+
 import java.util.Scanner;
-public class Tabung {
-    public static void main (String [] args) {
+
+public class Tabung extends BangunRuang{
+    double jari2, tinggi, phi=3.14;
+    public Tabung(){
+        System.out.println("Menghitung Tabung");
         Scanner nilai = new Scanner (System.in);
-        double phi= 3.14 , jari , tinggi ;
-        System.out.println("Masukan Jari-Jari = ");
-        jari = nilai.nextInt();
-        System.out.println("Masukan Tinggi = ");
+        System.out.print("\tMasukan Jari-Jari : ");
+        jari2 = nilai.nextInt();
+        System.out.print("\tMasukan Tinggi : ");
         tinggi = nilai.nextInt();
-        double Luas , Volume ;
-        Volume = phi * jari * jari * tinggi;
-        System.out.println("Volume Tabung adalah = "+Volume);
-        Luas = 2 * phi * jari * (jari + tinggi);
-        System.out.println("Luas Permukaan Tabung adalah = "+Luas);
+    }
+
+    @Override
+    public double volume() {
+        return this.phi * this.jari2 * this.jari2 * this.tinggi;
+    }
+
+    @Override
+    public double luasPermukaan() {
+        return 2 * this.phi * this.jari2 * (this.jari2 + this.tinggi);
     }
 }
